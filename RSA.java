27253d13b098635ff9b0261d81814733df8a6dc9
a01204421 <a01204421@itesm.mx>
2016-12-02 01:10:33 -0600

    otro archivo, modificando funciones del original RSA

--- RSA.java
+++ RSA.java
@@ -3,15 +3,14 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class RSA {
 
-	//Euclidean algorithm
+	//Algoritmo de euclides
 	public static int gcd(int x, int y) {
 		return y == 0 ? x : gcd(y, x % y);
 	}
 
-	//Extended Euclidean algorithm
+	//Algoritmo extendido de euclides
 	public static int multiplicative_inverse(int e, int phi) {
 		int d = 0, x = 0, y = 0, x1 = 0, x2 = 1, y1 = 1, temp_phi = phi, temp1 = 0, temp2 = 0;
-
 		while (e > 0) {
 			temp1 = temp_phi / e;
 			temp2 = temp_phi - temp1 * e;
@@ -26,76 +25,105 @@ public class RSA {
 			d = y1;
 			y1 = y;
 		}
-
 		/*
 		System.out.println("d: " + d);
 		System.out.println("phi: " + phi);
 		*/
-
-		return d;
+    return d;
 	}
 
-  //encripted function
+  //cifrar
 	public static int cipher(int M, int e, int n){
 		int PM = 0, exp = 0;
 		exp = (int) Math.pow(M, e);
 		PM = exp % n;
-
 		/*
 		System.out.println("exp: " + exp);
 		System.out.println("C: " + M);
 		System.out.println("n: " + n);
 		System.out.println("d: " + e);
 		*/
-
 		return PM;
 	}
 
-  //decripted function
+  //decifrar
 	public static long decipher(int C, int d, int n){
 		long SC = 0, exp = 0;
 		exp = (long) Math.pow(C, d);
 		SC = exp % n;
-
 		/*
 		System.out.println("exp: " + exp);
 		System.out.println("C: " + C);
 		System.out.println("n: " + n);
 		System.out.println("d: " + d);
 		*/
-
 		return SC;
 	}
 
+  //verificar si un numero es primo o no
+  public static boolean isPrime(int n) {
+      //validar si es multiplo de dos
+      if (n%2==0){
+        return false;
+      }
+      //si no, verificar si es multiplo de otros
+      for(int i=3;i*i<=n;i+=2) {
+          if(n%i==0){
+              return false;
+          }
+      }
+      return true;
+  }
+
+  public static int get_e(int p,int q){
+      int n=0, phi=0,e=0, maxdiv = 0,min = 1, max = 0;
+      n = p*q;
+      phi = (p-1)*(q-1);
+      if((isPrime(p) != true)&& (isPrime(q) != true)){
+          System.out.println("p y q deben ser numeros primos");
+      }else if(p == q){
+          System.out.println("p y q no pueden ser iguales");
+      }
+      max = phi;
+      e = ThreadLocalRandom.current().nextInt(min, max + 1);
+      maxdiv = gcd(e,phi);
+      while(maxdiv != 1){
+          e = ThreadLocalRandom.current().nextInt(min, max + 1);
+          maxdiv = gcd(e,phi);
+      }
+      return e;
+  }
+
 	public static void main(String[] args) {
-		/*Declaration of variables */
+		/*Declaracion de variables*/
 		int p = 0, q = 0, n = 0, e = 0, phi = 0, option = 0, result = 0, C = 0, M = 0, min = 2, max = 0, d = 0, count = 0;
 		double g = 0;
 		long result_decipher = 0;
-
-		//Keyboard
+		//Objeto teclado
 		Scanner keyboard = new Scanner(System.in);
 
-		/* Ask for prime numbers */
-		System.out.println("Give me p");
+		/* Pedir numeros*/
+		System.out.println("Dame p:");
 		p = keyboard.nextInt();
-		System.out.println("Give me q");
+		System.out.println("Dame q:");
 		q = keyboard.nextInt();
 
-		//Get n
+		//Obtener n
 		n = p * q;
 
-		//Get phi
+		//Obtener phi
 		phi = (p - 1) * (q - 1);
 
-		//Range for e variable
+		//Rango para la variable e
 		max = phi;
 
-		//If d is negative the Euclidean algorithm doesn't work
+		//Si el d es negativo el algoritmo euclidiano no trabaja
 		while (d <= 0) {
 
-			//Until pass Euclidean algorithm
+			//hasta que pasa el algoritmo euclidiano
 			while (g != 1) {
+        //e = get_e(p,q);
+        //e = 7;
 				e = ThreadLocalRandom.current().nextInt(min, max + 1);
 				g = gcd(e, phi);
 			}
@@ -104,8 +132,8 @@ public class RSA {
 
 			count++;
 
-			if(count > 60) {
-				System.out.println("Error with \"d\", is negative! Try again!");
+			if(count > 1000) {
+				System.out.println("Error al calcular \"d\", no puede ser negativo, prueba otra ves");
 				option = 3;
 				break;
 			}
@@ -120,31 +148,30 @@ public class RSA {
 		System.out.println("n: " + n);
 		*/
 
+    System.out.println("P = ("+e+","+n+")");
 		while (option != 3) {
-			System.out.println("Menu\n1. Cipher a message.\n2. Decipher a message.\n3. Exit.\nOption?");
+			System.out.println("Menu\n1. Cifrar un numero.\n2. Decifrar numero.\n3. Salir.\nOpcion?");
 			option = keyboard.nextInt();
 
 			switch (option) {
-			case 1:
-				System.out.println("Give the message to cipher");
-				M = keyboard.nextInt();
-				result = cipher(M, e, n);
-				System.out.println("Message ciphered: " + result);
-				break;
-
-			case 2:
-				System.out.println("Give the message to decipher");
-				C = keyboard.nextInt();
-				result_decipher = decipher(C, d, n);
-				System.out.println("Message deciphered: " + result_decipher);
-				break;
-
-			case 3:
-				System.out.println("Bye");
-				break;
-
-			default:
-				System.out.println("Choose a valid option");
+  			case 1:
+  				System.out.println("Escribe el numero a cifrar");
+  				M = keyboard.nextInt();
+  				result = cipher(M, e, n);
+  				System.out.println("Mensaje cifrado: " + result);
+  			break;
+  			case 2:
+          System.out.println("S = ("+d+","+n+")");
+  				System.out.println("Escribe el numero de decifrar");
+  				C = keyboard.nextInt();
+  				result_decipher = decipher(C, d, n);
+  				System.out.println("Mensaje decifrado: " + result_decipher);
+  			break;
+  			case 3:
+  				System.out.println("Adios");
+  			break;
+  			default:
+  				System.out.println("Escoje una opcion valida");
 				break;
 			}
 		}
---
+++ prueba.java
@@ -0,0 +1,186 @@
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class prueba {
+
+	//Algoritmo de euclides (no se usa)
+	public static int gcd(int x, int y) {
+		return y == 0 ? x : gcd(y, x % y);
+	}
+
+	//Algoritmo extendido de euclides (No se usa)
+	public static int euclidesExtendido(int a, int b){
+        int d=0,x=0,y=0;
+        if(b==0){
+            d=(int) a;
+            x=1;
+            y=0;
+            return d;
+        }
+        int x2 = 1, x1 = 0, y2 = 0, y1 = 1;
+        int q=0, r=0;
+        while(b>0)
+        {
+            q = (a/b);
+            r = a%b;
+            x = (int) (x2-q*x1);
+            y = (int) (y2-q*y1);
+            a = b;
+            b =  r;
+            x2 = x1;
+            x1 = x;
+            y2 = y1;
+            y1 = y;
+            d= a;
+        }
+        x = x2;
+        y = y2;
+        return d;
+	}
+
+  //inverso multiplicativo (No se usa)
+  public static int CalcularInverso(int n,int z){
+        int mcd = 0;
+        int x=0,y = 0;
+        if(n >z){
+            mcd= euclidesExtendido(n,z);
+        }
+        if(n< z){
+            mcd= euclidesExtendido(z,n);
+        }
+        System.out.println("MCD: "+mcd);
+        if(mcd >1){
+            System.out.println("EL INVERSO NO EXISTE");
+            y=0;
+        }
+        else{
+            y= mcd;
+            if(y< 0)
+            {
+                y=(y+z);
+            }
+        }
+        //System.out.println("Hola y:" +y);
+        return y;
+    }
+
+  //cifrar
+	public static int cipher(int M, int e, int n){
+		int PM = 0, exp = 0;
+		exp = (int) Math.pow(M, e);
+		PM = exp % n;
+		return PM;
+	}
+
+  //decifrar
+	public static long decipher(int C, int d, int n){
+		long SC = 0, exp = 0;
+		exp = (long) Math.pow(C, d);
+		SC = exp % n;
+		return SC;
+	}
+
+  //verificar si un numero es primo o no
+  public static int isPrime(int n) {
+      int iCount;
+      iCount = 2;
+      //validar si es multiplo de dos
+      if (iCount<2){
+        return 0;
+      }
+      //si no, verificar si es multiplo de otros
+      while(iCount<=(n/2)){
+          if(n%iCount==0)
+              return 0;
+          iCount++;
+      }
+      return 1;
+  }
+
+  public static int fnFindE(int a){
+      int iCount;
+      iCount=2;
+      while(iCount<a){
+          if(a%iCount!=0)
+              return iCount;
+          iCount++;
+      }
+      return 0;
+  }
+
+  public static int fnFindD(int a,int b){
+      int a1,a2,a3,b1,b2,b3,t1,t2,t3,q;
+      a1=1;
+      a2=0;
+      a3=a;
+      b1=0;
+      b2=1;
+      b3=b;
+      while(b3!=1){
+          q=a3/b3;
+
+          t1=a1-(q*b1);
+          t2=a2-(q*b2);
+          t3=a3-(q*b3);
+
+          a1=b1;
+          a2=b2;
+          a3=b3;
+
+          b1=t1;
+          b2=t2;
+          b3=t3;
+      }
+      if(b2<0)
+          b2=b2+a;
+      return b2;
+  }
+
+  public int fnFindText(int a,int b,int c){
+      int iCount,t;
+      iCount=1;
+      t=1;
+      while(iCount<=b){
+          t=t*a;
+          t=t%c;
+          iCount++;
+      }
+      return (t%c);
+  }
+
+	public static void main(String[] args) {
+		/*Declaracion de variables*/
+		int p = 0, q = 0, n = 0, e = 0, phi = 0, result = 0, C = 0, M = 0, min = 2, max = 0, d = 0, count = 0;
+		double g = 0;
+		long result_decipher = 0;
+		//Objeto teclado
+		Scanner keyboard = new Scanner(System.in);
+
+		/* Pedir numeros*/
+		System.out.println("Dame p:");
+		p = keyboard.nextInt();
+		System.out.println("Dame q:");
+		q = keyboard.nextInt();
+    if((isPrime(p) == 1) && (isPrime(q)) == 1){
+  		//Obtener n
+  		n = p * q;
+
+  		//Obtener phi
+  		phi = (p - 1) * (q - 1);
+      //obtener e
+      e = fnFindE(phi);
+			//obtener d
+      d = fnFindD(phi,e);
+      System.out.println("P = ("+e+","+n+")");
+  		System.out.println("Escribe el numero a cifrar");
+  		M = keyboard.nextInt();
+  		result = cipher(M, e, n);
+  		System.out.println("Mensaje cifrado: " + result);
+      System.out.println("S = ("+d+","+n+")");
+  		result_decipher = decipher(result, d, n);
+  		System.out.println("Mensaje decifrado: " + result_decipher);
+    }else{
+        System.out.println("Error! Enter two prime numbers!");
+    }
+	}
+}
